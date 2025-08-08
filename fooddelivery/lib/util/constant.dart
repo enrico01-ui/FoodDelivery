b085a995b53e38fa5f36cf8379dfbabe0a2e0c9c
Emanuel Enrico Anindya Wibawa <220711890@students.uajy.ac.id>
2025-08-08 18:22:22 +0700

    update api

--- fooddelivery/lib/util/constant.dart
+++ fooddelivery/lib/util/constant.dart
@@ -3,5 +3,5 @@ import 'package:flutter/material.dart';
 const SizedBox b1 = SizedBox(
   height: 24.0,
 );
-const String googleApiKey = 'AIzaSyDW2BiApKUjT01HHWAtKk6GYxV-jSe1p4s';
-const String baseIp = 'aplikasibioskop.web.id';
+const String googleApiKey = 'AIzaSyCcH6GBN8T8LfBo0flgPzGezmdcwY5Lnbw';
+const String baseIp = 'api.aplikasibioskop.web.id';
